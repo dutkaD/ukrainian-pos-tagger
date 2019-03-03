af602003df1a5924cd6dc51b7c2498e69b945290
Diana Dutka <diana.dutka@gmail.com>
2019-03-03 18:43:50 +0100

    tag and evaluate files

---
+++ tag.py
@@ -0,0 +1,34 @@
+# import operator
+# import sys
+# from collections import defaultdict
+#
+# from keras.models import load_model
+# import pickle
+# import numpy as np
+# from keras.preprocessing.sequence import pad_sequences
+# from keras.utils.np_utils import to_categorical
+# from nltk import SnowballStemmer
+# # from ukr_stemmer.ukr_stemmer3 import UkrainianStemmer
+
+DATA = "model/data.pkl"
+MODEL = "bestForwardTagger.h5"
+OUT = "tagged.txt"
+STEMMER = False
+LANGUAGE = "ua"
+
+
+
+
+# model = load_model('best_models/' + MODEL)
+
+
+
+
+
+
+
+# stats.show_results()
+
+
+
+
