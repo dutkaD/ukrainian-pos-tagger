d13d397dd9a311f990b408da289abbf16d2bff7a
Diana Dutka <diana.dutka@gmail.com>
2019-03-09 13:28:49 +0100

    evaluation

--- tagandeval.py
+++ tagandeval.py
@@ -1,22 +1,47 @@
+import operator
+import sys
+from collections import defaultdict
 from keras.models import load_model
 import pickle
 import numpy as np
 from keras.preprocessing.sequence import pad_sequences
+from nltk import SnowballStemmer
 from ukr_stemmer.ukr_stemmer3 import UkrainianStemmer
 
-DATA = "pickledSTEM.pkl"
+DATA = "pickledSTEM"
+MODEL = "stemmed_no_embeddingsboth-005.h5"
+out = "result.txt"
 TEST_DATA = "clean_test.txt"
-MODEL = "bestStemmedBoth.h5"
-OUT = "tagged.txt"
-STEMMER = True
 model = load_model(MODEL)
 
 
+STEMMER = True  # False if the complete forms of the words should be passed to the tagger
+LANGUAGE = "ua"  # "ru" for Russian. "ua" for Ukrainian
+
+tags_correct = 0
+sentences_correct = 0
+overall_tags = 0
+overall_sentences = 0
+counter = 0
+
+def evaluate(correct_tags, predicted):
+	correct = 0
+	sentece = False
+	err = False
+	for i in range(len(correct_tags)):
+		try:
+			if correct_tags[i] == predicted[i]:
+				correct += 1  # TODO: add a function that counts the result matrix ans saves the result as a pickle file
+			if correct == len(predicted):
+				sentece = True
+		except IndexError:
+			pass
+	return correct, sentece
 
 
 def write_tagged(words, tags):
 	sentence = ""
-	with open(OUT, "a") as f:
+	with open(out, "a") as f:
 		for i in range(len(words)):
 			try:
 				sentence += words[i] + "/" + tags[i] + " "
@@ -38,8 +63,7 @@ def get_predicted_tags(prediction, tokenized):
 
 	return predicted
 
-
-with open(DATA, 'rb') as f:
+with open(DATA + '.pkl', 'rb') as f:
 	X_train, Y_train, word2int, int2word, tag2int, int2tag, tag2instances = pickle.load(f)
 
 	del X_train
@@ -47,8 +71,6 @@ with open(DATA, 'rb') as f:
 
 with open(TEST_DATA) as test_f:
 	test_corpus = test_f.readlines()
-
-
 
 """
 Make two lists: tags and words to be used for later evaluation
@@ -77,15 +99,40 @@ for i in range(len(words_pro_sent)):
 	for word in words_pro_sent[i]:
 		try:
 			if STEMMER:
-				stemObj = UkrainianStemmer(word)
-				word = stemObj.stem_word()
+				if LANGUAGE == "ru":
+					stemmer = SnowballStemmer("russian")
+					word = stemmer.stem(word)
+				if LANGUAGE == "ua":
+					stemObj = UkrainianStemmer(word)
+					word = stemObj.stem_word()
 			tokenized_sentence.append(word2int[word])
 
 		except KeyError:
 			tokenized_sentence.append(word2int["<UNKNOWN>"])  # if the word was not found in the dictionary
 
 	np_tokenized = np.asarray([tokenized_sentence])
+
 	padded_tokenized_sentence = pad_sequences(np_tokenized, maxlen=100)
 	prediction = model.predict(padded_tokenized_sentence)
 	predicted_tags = get_predicted_tags(prediction, words_pro_sent[i])
+	corr, sent = evaluate(correct_tags[i], predicted_tags)
+
+	overall_tags += len(correct_tags[i])
+	overall_sentences += 1
+	tags_correct += corr
+	if sent:
+		sentences_correct += 1
+
 	write_tagged(words_pro_sent[i], predicted_tags)
+	print(i)
+
+
+print("{}/{} - {}".format(tags_correct, overall_tags, round(tags_correct/overall_tags*100, 2)))
+print("{}/{} - {}".format(sentences_correct, overall_sentences, round(sentences_correct/overall_sentences*100, 2)))
+
+
+# stats.show_results()
+
+
+
+
